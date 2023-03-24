ad45efca55fb144c103a6e40c3457004cb829d68
lucdainez <lucdainez@gmail.com>
2023-03-24 16:31:13 -0300

    feat: def filter_by_industry

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -9,18 +9,5 @@ def get_unique_industries(path: str) -> List[str]:
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
-    """Filters a list of jobs by industry
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    raise NotImplementedError
+    list_filtered_industry = [i for i in jobs if i["industry"] == industry]
+    return list_filtered_industry
