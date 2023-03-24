c9e7f33b79fc8d0c848d4eb85718e9d648e9370e
lucdainez <lucdainez@gmail.com>
2023-03-24 18:53:44 -0300

    feat: def filter_by_salary_range implementada

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -29,46 +29,34 @@ def get_min_salary(path: str) -> int:
 
 
 def matches_salary_range(job: Dict, salary: Union[int, str]) -> bool:
-    """Checks if a given salary is in the salary range of a given job
+    try:
+        maximun_salary = int(job["max_salary"])
+        minimun_salary = int(job["min_salary"])
+        salary_parameter = int(salary)
 
-    Parameters
-    ----------
-    job : dict
-        The job with `min_salary` and `max_salary` keys
-    salary : int
-        The salary to check if matches with salary range of the job
+    except TypeError:
+        raise ValueError(
+            "salary, maximun_salary e minimun_salary devem ser numéricos"
+        )
 
-    Returns
-    -------
-    bool
-        True if the salary is in the salary range of the job, False otherwise
+    except KeyError:
+        raise ValueError("chave inexistente")
 
-    Raises
-    ------
-    ValueError
-        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-        If `job["min_salary"]` is greather than `job["max_salary"]`
-        If `salary` isn't a valid integer
-    """
-    raise NotImplementedError
+    if minimun_salary > maximun_salary:
+        raise ValueError("min_salary é maior que o max_salary")
+
+    return minimun_salary <= salary_parameter <= maximun_salary
 
 
 def filter_by_salary_range(
     jobs: List[dict], salary: Union[str, int]
 ) -> List[Dict]:
-    """Filters a list of jobs by salary range
+    list_of_salaries = []
 
-    Parameters
-    ----------
-    jobs : list
-        The jobs to be filtered
-    salary : int
-        The salary to be used as filter
-
-    Returns
-    -------
-    list
-        Jobs whose salary range contains `salary`
-    """
-    raise NotImplementedError
+    for i in jobs:
+        try:
+            if matches_salary_range(i, salary):
+                list_of_salaries.append(i)
+        except ValueError:
+            pass
+    return list_of_salaries
