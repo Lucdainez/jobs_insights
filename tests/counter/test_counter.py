a3e02f100383fad8e16c4dc15fef30ff6c63991d
lucdainez <lucdainez@gmail.com>
2023-03-25 17:17:52 -0300

    test: def count_ocurrences testada

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,9 @@
-# from src.pre_built.counter import count_ocurrences
+# import pytest
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    assert count_ocurrences("data/jobs.csv", "JOB") == 3454
+    assert count_ocurrences("data/jobs.csv", "job") == 3454
+    assert count_ocurrences("data/jobs.csv", "business") == 4709
+    assert count_ocurrences("data/jobs.csv", "business") != 47
