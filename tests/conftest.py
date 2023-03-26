eab94d29e595ddb6af939a8ed2a6ad7cba10beea
lucdainez <lucdainez@gmail.com>
2023-03-26 19:37:52 -0300

    delete: retirando arquivos de autoria da trybe

--- tests/conftest.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from src.insights.jobs import read
-
-
-@pytest.fixture(scope="function", autouse=True)
-def clear_cache():
-    read.cache_clear()
--- tests/counter/mocks.py
+++
@@ -1,5 +0,0 @@
-def count_word_ocurrences(path, word):
-    file = open(path, "r")
-    read_data = file.read()
-    word_count = read_data.count(word)
-    return word_count
